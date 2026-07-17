afdb0146947a93b92d1a3a7519304b126ca447c7
Pedro Henrique Penna <ppenna@microsoft.com>
2026-07-17 12:27:22 -0700

    [Nanvix] Allow unresolved symbols in shared objects
    Nanvix shared runtimes resolve loader and process-image symbols when they are loaded. Exclude Nanvix from the generic ELF -z defs policy so libc++abi and the other runtime DSOs can retain those references.

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -326,7 +326,8 @@ endif()
 
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
-if(NOT (CMAKE_SYSTEM_NAME MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX|OS390|Emscripten" OR
+# Nanvix shared objects resolve process-image symbols when they are loaded.
+if(NOT (CMAKE_SYSTEM_NAME MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX|OS390|Emscripten|Nanvix" OR
         WIN32 OR CYGWIN) AND
    NOT LLVM_USE_SANITIZER)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
